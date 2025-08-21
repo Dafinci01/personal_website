0cdcaeaad9572368aa0d6a5579e498995af8a42d
Dafinci01 <Davehacker38@gmail.com>
2025-08-21 12:09:34 +0100

    feat: added IntoAnimation and Routes Pages

--- my-portfolio_website/src/App.jsx
+++ my-portfolio_website/src/App.jsx
@@ -1,14 +1,16 @@
 import React from'react';
+import IntroAnimation  from "./components/IntroAnimation";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
 import About from "./pages/About";
 import Project from "./pages/Project";
-import NavBAr  from "./assets/component/NavBAr";
+import NavBar  from "./assets/components/NavBar";
 function App() {
     return (
         <BrowserRouter>
+            <IntroAnimation/>
             <NavBar/>
             <Routes>
                 <Route path="/" element={<Home />} />
--- my-portfolio_website/src/assets/components/NavBar.jsx
+++ my-portfolio_website/src/assets/components/NavBar.jsx
@@ -1,9 +1,19 @@
-  import React from 'react'
+  import React from 'react';
+
   
   function NavBar() {
-    return (
-      <div>NavBar</div>
-    )
+    return <header className = "bg-white shadow-md ">
+      <>
+      <div className= "py-4 shadow-md">
+        <ul className = "container mx-auto  flex flex-wrap justify-between md:flex-row px-4 ">
+            
+        </ul>
+
+      </div>
+
+      </>
+
+    </header>
   }
   
-  export default NavBar
+  export default NavBar;
